f62f883731fcb8f425a5d80542471df301634306
Raúl Soriano <GatoLoko@gmail.com>
2009-11-23 22:51:00 +0100

    * autosend.py: modificados varios mensajes para hacerlos mas genericos    y correctos.  * gatoscript.conf: cambiado el disparador para el autoenvio.

--- gatoscript/modulos/autosend.py
+++ gatoscript/modulos/autosend.py
@@ -87,20 +87,20 @@ def torrent_cb(word, word_eol, userdata):
     if activo == True:
         #Definimos la expresion regular que actuara como disparador
         texto = auxiliar.lee_conf("autosend", "disparador")
-        disparador = re.compile(texto, re.IGNORECASE)
+        disparador = re.compile("^" + texto, re.IGNORECASE)
         if word[2] in protegidos:
             if disparador.search(word[3][1:]):
                 partes = word_eol[3].split()
                 if len(partes) == 1:
                     lista = listdir(almacen)
                     if len(lista) == 0:
-                        xchat.command("say Aun no hay torrents disponibles")
+                        xchat.command("say No hay archivos disponibles")
                     elif len(lista) > 0:
                         archivos = ""
                         for archivo in lista:
                             archivos += archivo + " "
-                        xchat.command("say Tengo los siguientes torrents: " + archivos)
-                        xchat.command("say Para pedir uno pon: !torrent nombre")
+                        xchat.command("say Tengo los siguientes archivos: " + archivos)
+                        xchat.command("say Para pedir uno pon: %s archivo" % texto)
                 elif len(partes) == 2:
                     lista = listdir(almacen)
                     if partes[1] in lista:
@@ -109,7 +109,7 @@ def torrent_cb(word, word_eol, userdata):
                     else:
                         xchat.command("say No tengo ese archivo, revisa la lista")
                 elif len(partes) > 2:
-                    xchat.command("say ¡No seas abuson! ¡Un solo archivo!")
+                    xchat.command("say ¡No seas abuson! ¡Un solo archivo cada vez!")
                 
     return xchat.EAT_NONE
     
