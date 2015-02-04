ad65e9295702547b26f32752f3490583ee63a089
Raúl Soriano <GatoLoko@gmail.com>
2015-02-04 21:22:08 +0100

    Add commands modulue to take them out of the helper
    The helper module should not contain any interactive command, just
    helper functions for the script, so this one will contain those
    interactive commands that dont fit in any other module.

---
+++ gatoscript/gmodules/commands.py
@@ -0,0 +1,69 @@
+# -*- coding: UTF8 -*-
+
+# CopyRight (C) 2006-2013 GatoLoko
+#
+# This file is part of GatoScript
+#
+# GatoScript is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# GatoScript is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GatoScript; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+"""
+GatoInfo module for GatoScript.
+
+This module contains informational functions about GatoScript itself.
+"""
+
+__module_name__ = "GatoScript Commands"
+__module_version__ = "2.0-alpha"
+__module_description__ = "Commands module for GatoScript"
+__module_autor__ = "GatoLoko"
+
+# Load all needed libraries
+import xchat
+from . import helper
+
+#############################################################################
+# Define some environment variables
+#############################################################################
+
+#############################################################################
+# Initialize the module
+#############################################################################
+
+
+#############################################################################
+# Define functions for GatoScript
+#############################################################################
+
+
+#############################################################################
+# Define the help function
+#############################################################################
+
+
+#############################################################################
+# Define the function to unload this module. This should be called from the
+# main module unload function
+#############################################################################
+
+
+#############################################################################
+# Connect all HexChat/X-Chat hooks with the functions defined for them
+#############################################################################
+
+
+
+#############################################################################
+# Add Information and Options menus
+#############################################################################
